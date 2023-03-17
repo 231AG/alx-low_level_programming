69d6efc6e97cfd146503bb74e7a1bf8d6f11565a
Foday Malike Kabah <fodaykabah1997@gmail.com>
2023-03-17 07:35:14 -0700

    Task 6 completed

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints from 0 to 9
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		putchar('%d\n', i)
+	}
+	putchar('\n')
+	return (0)
+}
