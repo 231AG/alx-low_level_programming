4ea3cf410130eb584ac2067de40ec0696e91778f
Foday Malike Kabah <fodaykabah1997@gmail.com>
2023-03-17 07:10:06 -0700

    Task 5 completed

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/*
+ * Main - Entry point
+ * Return: Always 0 (Sucess)
+ * /
+ int main(void) 
+ {
+ 	int a = 0;
+	while (a < 10)
+	{
+		printf("%d", a);
+		a++;
+	}
+	return (0);
+ }
