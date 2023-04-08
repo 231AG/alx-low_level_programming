64385ef110daecab7acca17c7782431098349c9d
Foday Malike Kabah <fodaykabah1997@gmail.com>
2023-04-08 13:54:00 -0700

    Betty Style fixed

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -2,19 +2,17 @@
 /**
  * _strlen - returns the length of a string
  * @s: string
- * return: length
+ * Return: length
  */
 int _strlen(char *s)
 {
 	int longi = 0;
-
 
 	while (*s != '\0')
 	{
 		longi++;
 		s++;
 	}
-
 
 	return (longi);
 }
