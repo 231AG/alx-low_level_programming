ff7111b64dde270dd08b77fa1c21176a687bdd01
Foday Malike Kabah <fodaykabah1997@gmail.com>
2023-07-08 13:18:43 -0700

    init commit

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,39 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - function to create a hash table
+ * @size: the size of array
+ *
+ * Return: the result.
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash = NULL;
+	hash_node_t **ar = NULL;
+	unsigned long int x = 0;
+
+	hash = malloc(sizeof(hash_table_t));
+
+	if (hash == NULL)
+	{
+		return (NULL);
+	}
+
+	ar = malloc(sizeof(hash_node_t *) * size);
+
+	if (ar == NULL)
+	{
+		return (NULL);
+	}
+
+	for (; x < size; ++x)
+	{
+		ar[x] = NULL;
+	}
+
+	hash->size = size;
+	hash->array = ar;
+
+	return (hash);
+}
