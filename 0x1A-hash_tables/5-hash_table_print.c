4a760f1e96d56b69fbbf3778b1b448d172fd7882
Foday Malike Kabah <fodaykabah1997@gmail.com>
2023-07-08 13:50:06 -0700

    tAsk 6

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,40 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print -print a hash table
+ * @ht: pointer to checked
+ *
+ * Return: nothing
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *index = NULL;
+	unsigned long int x;
+	short name = 0;
+
+	if (ht == NULL)
+	{
+		return;
+	}
+
+	printf("{");
+
+	for (x = 0; x < ht->size; ++x)
+	{
+		index = ht->array[x];
+
+		while (index)
+		{
+			if (name == 1)
+			{
+				printf(", ");
+			}
+			name = 1;
+			printf("'%s': '%s'", (char *) index->key, (char *) index->value);
+			index = index->next;
+
+		}
+	}
+	printf("}\n");
+}
